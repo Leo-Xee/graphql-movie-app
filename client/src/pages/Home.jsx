81ef3de7e04a2c68d324035670eda93c79598d6a
Leo-Xee <jang.xmin@gmail.com>
2022-07-12 19:19:41 +0900

    feat: Home 구현

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,34 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { Link } from "react-router-dom";
+
+const GET_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      id
+      title
+    }
+  }
+`;
 
 function Home() {
-  return <div>Home</div>;
+  const { data, loading, error } = useQuery(GET_MOVIES);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error!!</h1>;
+
+  return (
+    <div>
+      <h1>Movie List</h1>
+      <ul>
+        {data.allMovies.map((movie) => (
+          <li key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
