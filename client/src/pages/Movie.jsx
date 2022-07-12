fcc4b68681d8c6d467d746c49ac2c438079a2eea
Leo-Xee <jang.xmin@gmail.com>
2022-07-12 19:19:53 +0900

    feat: Movie 구현

--- client/src/pages/Movie.jsx
+++ client/src/pages/Movie.jsx
@@ -1,7 +1,28 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: ID!) {
+    movie(id: $movieId) {
+      id
+      title
+    }
+  }
+`;
 
 function Movie() {
-  return <div>Movie</div>;
+  const { id } = useParams();
+  console.log("id", typeof id, id);
+  const { data, loading } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: id,
+    },
+  });
+
+  if (loading) return <h1>Fetching...</h1>;
+
+  return <div>{data.movie.title}</div>;
 }
 
 export default Movie;
